1d99245eb68433da00a6e43749798c89c35e5daa
LeeX <a543748616@gmail.com>
2018-10-30 00:43:59 +0800

    1.1练习部分完成

--- practice/chapter1/1.1/fuction.hs
+++ practice/chapter1/1.1/fuction.hs
@@ -1,1 +1,62 @@
--- 这是关于调用函数的练习
+-- 17
+2 + 15
+-- 57
+3 * 19
+-- -2
+3827 - 3829
+-- 2.5
+5 / 2
+
+-- 1468
+(50 * 30) - 32
+-- 9343
+302 * 32 - 321
+-- -38556
+84 * (53 - 512)
+
+-- False
+True && False
+-- True
+True && True
+-- True
+False || True
+-- True
+not False
+False
+not (True && True)
+
+-- True
+5 == 5
+-- False
+1 == 0
+-- False
+5 /= 5
+-- True
+5 /= 4
+-- True
+"hello " /= "hello"
+
+-- 9
+succ 8
+
+-- 9
+min 9 10
+-- 11
+max 11 5
+-- 3.2
+min 3.5 3.2
+
+-- 105
+succ 9 * 10 + max 3 4 + 1
+-- 16
+(succ 9) + (max 5 4) + 1
+
+-- 90
+succ 8 * 10
+-- 91
+succ (9 * 10)
+
+-- 3
+div 32 10
+-- 3
+32 `div` 10
